c7c86b030517ffb5e8c2fa82d3aec1c550b8b5fc
Harry <hlbarber93@gmail.com>
2018-12-07 14:57:58 +0700

    Add Hamming distance

--- src/tests/test_byte_tools.rs
+++ src/tests/test_byte_tools.rs
@@ -25,4 +25,15 @@ use utils::byte_ops::*;
         assert_eq!(Bytes::bitxor(raw_a, raw_b), result)
     }
 
-    
+    #[test]
+    fn test_hamming_weight(){
+        let raw = Bytes::from(&b"\x00\x01\x00\x01"[..]);
+        assert_eq!(raw.hamming_weight(), 2);
+    }
+
+    #[test]
+    fn test_hamming_distance(){
+        let raw_a = Bytes::from(&b"\x00\x01\x00\x01"[..]);
+        let raw_b = Bytes::from(&b"\x00\x01\x01\x01"[..]);
+        assert_eq!(Bytes::hamming_distance(raw_a, raw_b), 1);
+    }    
--- src/utils/byte_ops.rs
+++ src/utils/byte_ops.rs
@@ -2,6 +2,7 @@ extern crate bytes;
 use bytes::{Bytes, Buf, BufMut, IntoBuf};
 use std::ops::*;
 
+
 macro_rules! bop {
 	($trait_name: ident, $fn_name: ident) => (
 		pub trait $trait_name {
@@ -31,3 +32,29 @@ macro_rules! bop {
 bop!(BitAndByte, bitand);
 bop!(BitOrByte, bitor);
 bop!(BitXorByte, bitxor);
+
+pub trait Hamming {
+	fn hamming_weight(&self) -> u32;
+	fn hamming_distance(self, Bytes) -> u32;
+}
+
+impl Hamming for Bytes {
+	fn hamming_weight(&self) -> u32 {
+		let len = self.len();
+
+		let mut count = 0;
+		let mut buf = self.into_buf();
+
+		let mut current: u8;
+		for _i in 0..len {
+			current = buf.get_u8();
+			count += current.count_ones();
+		}
+
+		count
+	}
+
+	fn hamming_distance(self, rhs: Bytes) -> u32 {
+		Bytes::bitxor(self, rhs).hamming_weight()
+	}
+}
