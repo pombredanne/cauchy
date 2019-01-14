022b12d6eb5eeb9455cd3600a3651f19314eab1b
Bluesun <c.r.novick@gmail.com>
2019-01-13 19:02:39 -0500

    Move tests to appropriate loc

--- ch-vm/src/vm.rs
+++ ch-vm/src/vm.rs
@@ -6,22 +6,20 @@ use ckb_vm::{
 
 use ckb_vm::memory::{ Memory};
 use std::vec::Vec;
+use std::fs::File;
+use std::io::Read;
 
 pub struct VM {
     ret_bytes : Vec<u8>
-    // pub machine: ckb_vm::DefaultMachine<'static, u64, SparseMemory>,
 }
 
 impl VM {
     pub fn new() -> VM {
         VM {
-            // machine: ckb_vm::DefaultMachine::<u64, SparseMemory>::default(),
             ret_bytes : vec![],
         }
     }
-    // pub fn init(&mut self) {
-    //     self.machine.add_syscall_module(Box::new(VMSyscalls { }));
-    // }
+
     pub fn run(&mut self, buffer: &[u8], args : &[Vec<u8>]) -> Result<u8, Error> {
         // self.machine.run(buffer, args)
         let mut machine = ckb_vm::DefaultMachine::<u64, SparseMemory>::default();
@@ -30,6 +28,7 @@ impl VM {
         self.ret_bytes = machine.get_retbytes().to_vec();
         result
     }
+
     pub fn run_args(&mut self, buffer: &[u8], input_bytes : Vec<u8>) -> Result<u8, Error> {
         let len = input_bytes.len();
         let args = &vec![b"__vm_script".to_vec(), input_bytes, len.to_string().as_bytes().to_vec()];
@@ -77,4 +76,62 @@ impl Syscalls<u64, SparseMemory> for VMSyscalls {
             _ => Ok(false)
         }
     }
+}
+
+#[cfg(test)]
+
+#[test]
+fn test_simple() {
+    let mut buffer = Vec::new();
+    File::open("tests/simple").unwrap().read_to_end(&mut buffer).unwrap();
+
+    let mut vm = VM::new();
+    let result = vm.run(&buffer, &vec![b"__vm_script".to_vec()]);
+
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), 42);
+}
+
+#[test]
+fn test_vm_syscalls() {
+    let mut buffer = Vec::new();
+    File::open("tests/syscalls").unwrap().read_to_end(&mut buffer).unwrap();
+
+    let mut vm = VM::new();
+    let result = vm.run(&buffer, &vec![b"__vm_script".to_vec()]);
+    assert!(result.is_ok());
+    assert_eq!(result.unwrap(), 0);
+
+    let bytes = vm.get_retbytes();
+    assert_eq!(bytes, &vec![0x41, 0x42, 0x43, 0x44, 0x45, 0x46, 0x47, 0x48]);
+}
+
+#[test]
+fn test_sha256() {
+    let mut buffer = Vec::new();
+    File::open("tests/sha256").unwrap().read_to_end(&mut buffer).unwrap();
+
+    let mut vm = VM::new();
+    let result = vm.run(&buffer, &vec![b"__vm_script".to_vec()]);
+    assert!(result.is_ok());
+    let bytes = vm.get_retbytes();
+    assert_eq!(bytes, &hex::decode("e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855").unwrap());
+
+    // Now test a string as input
+    let input_bytes = b"hello".to_vec();
+    let result = vm.run_args(&buffer, input_bytes);
+    assert!(result.is_ok());
+    let bytes = vm.get_retbytes();
+    assert_eq!(bytes, &hex::decode("2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824").unwrap());
+
+    // Now test double sha256
+    let input_bytes = vm.get_retbytes().to_vec();
+    let result = vm.run_args(&buffer, input_bytes);
+    assert!(result.is_ok());
+    let bytes = vm.get_retbytes();
+    assert_eq!(bytes, &hex::decode("9595c9df90075148eb06860365df33584b75bff782a510c6cd4883a419833d50").unwrap());
+
+    let bytes = vm.get_retbytes();
+    println!("{:X?}", bytes);
+    
 }
