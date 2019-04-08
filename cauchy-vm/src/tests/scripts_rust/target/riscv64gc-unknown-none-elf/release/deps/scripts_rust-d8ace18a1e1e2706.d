766f7a421d93b41d185d887110a6db095115ba58
TheRealBluesun <c.r.novick@protonmail.com>
2019-04-08 13:55:28 -0400

    Cleanup build garbage

--- cauchy-vm/src/tests/scripts_rust/target/riscv64gc-unknown-none-elf/release/deps/scripts_rust-d8ace18a1e1e2706.d
+++
@@ -1,5 +0,0 @@
-/media/veracrypt1/cauchy/cauchy-vm/src/tests/scripts_rust/target/riscv64gc-unknown-none-elf/release/deps/scripts_rust-d8ace18a1e1e2706: src/main.rs
-
-/media/veracrypt1/cauchy/cauchy-vm/src/tests/scripts_rust/target/riscv64gc-unknown-none-elf/release/deps/scripts_rust-d8ace18a1e1e2706.d: src/main.rs
-
-src/main.rs:
--- cauchy-vm/src/tests/scripts_rust/target/riscv64gc-unknown-none-elf/release/scripts_rust.d
+++
@@ -1,1 +0,0 @@
-/media/veracrypt1/cauchy/cauchy-vm/src/tests/scripts_rust/target/riscv64gc-unknown-none-elf/release/scripts_rust: /media/veracrypt1/cauchy/cauchy-vm/src/tests/scripts_rust/src/main.rs
